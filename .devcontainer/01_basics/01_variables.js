e7c0ec8dd4256cb8456503ff2dc18cf8c2b2f5bb
Bidhan Bhandari <70732909+bidhan1000@users.noreply.github.com>
2024-01-25 06:22:06 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId="123"
+let accoutName="bidhan"
+let address="ktm"
+let email="abc.com"
+
+console.table([accountId,accoutName,address,email])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+//number=> 3 to power 78
+//bigint
+//string=> "abc"
+//boolean=> true/false
+//null=> standalone value(null is used instead of o) (null is an object)
+//undefined=>
+//symbol=> unique
+
+console.log(typeof undefined);
+console.log(typeof null);
