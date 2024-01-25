a9a452e9bf1fa0ca75124685997a08071124d287
Bidhan Bhandari <70732909+bidhan1000@users.noreply.github.com>
2024-01-25 16:08:30 +0000

    conversionof datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,8 @@
+let goal="43"
+console.log(typeof goal);
+let valueInNumber=Number(goal)
+console.log(typeof goal);
+
+// "43" => 43
+// "43abc" => NaN (Not a number)
+// true => 1; false => 0
---
+++ .devcontainer/01_basics/04_operation.js
@@ -0,0 +1,15 @@
+let value = 2
+let negValue = -value
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**4);
+
+let str1 = "hello"
+let str2 = "hellooo"
+let str3 = str1+str2    //Add string
+//console.log(str3);
+
+console.log(true);
+
