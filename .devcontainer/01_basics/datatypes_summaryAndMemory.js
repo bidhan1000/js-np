c8f4dc25bb804bec1f44b88385dda5ec85bba190
Bidhan Bhandari <70732909+bidhan1000@users.noreply.github.com>
2024-01-28 11:43:59 +0000

    datatypes_summary&emory

---
+++ .devcontainer/01_basics/datatypes_summaryAndMemory.js
@@ -0,0 +1,42 @@
+//Types of datatypes
+//Primitive datatypes
+//7types: ie. String, Number, Boolean, Null, undefined, symbol, BigInt
+
+const score="100"                // String
+const scorenumber=100.5        // Number
+
+const isloggedin="false"         // Boolean
+const Tempretue="null"           // Null
+let userEmail;                   // Undefined
+
+const id= Symbol('123')          // Symbol  (false output)
+const anotherID= Symbol('123')
+//console.log(id===anotherID);
+
+const bignumber = 4324234234234324324n        // BigInt  (represent n in last)
+
+
+//Non premitive (Reference datatypes)
+
+// 3 types
+// Array, Objects, Functions
+
+const heroes = ["spiderman", "batman","superman"];         //Array
+
+let myobj = {                     // Objectd (inside curly bracket)
+name: "bidhan",
+age: 43,
+}
+ 
+console.log(typeof scorenumber);
+
+
+//+++++++++++++++++++++++++++++++**************************************+++++++++++++++++++++++++++++++++
+
+//Types of memory
+// Stack memory(primitive), Heap memory(Non primitive)
+
+//.......................
+
+
+
